92a51f702ae201f6993f4c1c572c33d3b06ed86c
Emmanuel Solis <emma.ruso@outlook.com>
2023-02-17 07:59:31 -0600

    js changed

--- script.js
+++ script.js
@@ -15,18 +15,9 @@ const encabezadoColumna2 = filaEncabezado.insertCell();
 encabezadoColumna1.textContent = "Columna 1";
 encabezadoColumna2.textContent = "Columna 2";
 
-// Pedir al usuario que introduzca los valores para cada columna
-const datosColumna1 = [5, 2, 8, 10, 1, 6, 7, 9, 3, 4];
-const datosColumna2 = ["manzana", "banana", "naranja", "limón", "fresa", "melón", "piña", "uva", "sandía", "kiwi"];
-
-const cantidadDatos = prompt("¿Cuántos datos quieres introducir?");
-for (let i = 0; i < cantidadDatos; i++) {
-  const datoColumna1 = prompt("Introduce el valor para la columna 1:");
-  const datoColumna2 = prompt("Introduce el valor para la columna 2:");
-  
-  datosColumna1.push(datoColumna1);
-  datosColumna2.push(datoColumna2);
-}
+// Obtener los datos para las columnas
+const datosColumna1 = ["Dato 1", "Dato 2", "Dato 3"];
+const datosColumna2 = ["Valor 1", "Valor 2", "Valor 3"];
 
 // Crear filas de datos y agregarlas a la tabla
 for (let i = 0; i < datosColumna1.length; i++) {
